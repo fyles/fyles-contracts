a84dd75e96502ad3b71b924ed7b32fa4e16280bd
Marc Brown <heymarcbrown@gmail.com>
2018-10-25 15:19:08 -0400

    Clean up test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
  
-ganache-cli --gasLimit 6721975 2> /dev/null 1> /dev/null &
+ganache-cli --gasLimit 6721975 &> /dev/null &
 sleep 5 # ensure ganache-cli is up
-rm -rf build
+
 truffle compile
 truffle migrate --reset --network development
 truffle test
+
 kill -9 $(lsof -t -i:8545)
