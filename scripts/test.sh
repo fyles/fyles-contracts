77c42593723f49b21818eabc74423998d6320e9d
Marc Brown <heymarcbrown@gmail.com>
2018-10-27 12:24:34 -0400

    [CI] Add code coverage to contracts

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,6 @@
 ganache-cli --gasLimit 6721975 &> /dev/null &
 sleep 5 # ensure ganache-cli is up
 
-truffle compile
-truffle migrate --reset --network development
-truffle test
+yarn coverage && cat coverage/lcov.info | coveralls
 
 kill -9 $(lsof -t -i:8545)
